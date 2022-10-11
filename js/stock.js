14481b678ee02b13d045c48d015be62953c2abf8
barbara pino <barbargustavomili@gmail.com>
2022-10-11 01:02:18 -0300

    cambios

--- js/stock.js
+++ js/stock.js
@@ -1,9 +1,9 @@
 //objetos
 
 let stockProductos = [
-    {id: 1, nombre: "producto1", tipo: "pasteleria", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1200, opcion: "A", img: './img/pasteleria.png'},
-    {id: 2, nombre: "producto2", tipo: "panaderia salada", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1100, opcion: "B", img: './img/pul-2.png'},
-    {id: 3, nombre: "producto3", tipo: "panaderia dulce", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1200, opcion: "C", img: './img/pul-3.png'},
+    {id: 1, nombre: "producto1", tipo: "pasteleria", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1200, opcion: "A", img: 'git '},
+    {id: 2, nombre: "producto2", tipo: "panaderia salada", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1100, opcion: "B", img: '../img/pasteleria.png'},
+    {id: 3, nombre: "producto3", tipo: "panaderia dulce", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1200, opcion: "C", img: '../img/pasteleria.png'},
     {id: 4, nombre: "producto4", tipo: "tortas y tartas", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1400, opcion: "D", img: './img/pul-4.png'},
     {id: 5, nombre: "producto5", tipo: "pasteleria", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1200, opcion: "A", img: '../img/producto-4.png'},
     {id: 6, nombre: "producto6", tipo: "panaderia salada", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1100, opcion: "B", img: './img/pul-2.png'},
@@ -17,7 +17,7 @@ let stockProductos = [
     {id: 14, nombre: "producto15", tipo: "panaderia salada", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1100, opcion: "B", img: './img/pul-2.png'},
     {id: 16, nombre: "producto16", tipo: "panaderia dulce", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1200, opcion: "C", img: './img/pul-3.png'},
     {id: 16, nombre: "producto17", tipo: "tortas y tartas", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1400, opcion: "D", img: './img/pul-4.png'},
-    {id: 17, nombre: "producto18", tipo: "pasteleria", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1200, opcion: "A", img: '../img/producto-4.png'},
+    {id: 17, nombre: "producto18", tipo: "pasteleria", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1200, opcion: "A", img: './img/elaboration-3.png'},
     {id: 18, nombre: "producto19", tipo: "panaderia salada", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1100, opcion: "B", img: './img/pul-2.png'},
     {id: 19, nombre: "producto20", tipo: "panaderia dulce", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1200, opcion: "C", img: './img/pul-3.png'},
     {id: 20, nombre: "producto21", tipo: "tortas y tartas", cantidad: 1, desc: "RECETAS DULCES Y SALADAS PARA TODO MOMENTO Y OCASION", precio: 1400, opcion: "D", img: './img/pul-4.png'},
